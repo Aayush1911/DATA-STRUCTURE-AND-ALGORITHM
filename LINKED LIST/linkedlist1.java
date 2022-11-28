ae23d1e0c94dc0ac6c446113e4bc03f8f340b9f0
Aayush191102 <21it49@svitvasad.ac.in>
2022-11-28 22:59:08 +0530

    ADD FIRST , ADD LAST , PRINT IN LINKEDLIST

--- LINKED LIST/linkedlist1.java
+++ LINKED LIST/linkedlist1.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+
 public class linkedlist1 {
     class  Node{
         int data;
@@ -8,6 +10,43 @@ public class linkedlist1 {
         }
     }
     public static void main(String args[]){
+    public static Node head;
+    public static Node tail;
+        }
 
+
+        head = newnode;
+    public void lastadd(int data){
+        Node newnode = new Node(data);
+        if(head==null){
+            head = tail = newnode;
+            return;
+        }
+        tail.next = newnode;
+        tail = newnode;
     }
+public void print(){
+    if(head == null){
+        System.out.println("linkedlist is empty");
+        return;
+    }
+    Node temp = head;
+    while(temp!=null){
+        System.out.print(temp.data + " ->");
+        temp = temp.next;
+    }
+    System.out.println("NULL");
+}
+    public static void main(String args[]){
+        linkedlist1 ll = new linkedlist1();
+        ll.print();
+        ll.addfirst(1);
+        ll.print();
+        ll.addfirst(2);
+        ll.print();
+        ll.lastadd(3);
+        ll.print();
+        ll.lastadd(4);
+        ll.print();
+}
 }
